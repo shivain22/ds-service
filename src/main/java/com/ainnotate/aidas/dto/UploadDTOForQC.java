98bddb65571827f92b16cc3a1096b51a5e922748
shiva <shivain22@gmail.com>
2023-04-27 06:49:18 +0530

    qc bug fixes

---
+++ src/main/java/com/ainnotate/aidas/dto/UploadDTOForQC.java
@@ -0,0 +1,135 @@
+package com.ainnotate.aidas.dto;
+
+public class UploadDTOForQC {
+
+	private Long uploadId;
+	private String uploadUrl;
+	private String projectName;
+	private String objectName;
+	private Long userVendorMappingObjectMappingId;
+	private Long projectId;
+	private Long objectId;
+	private Long ucbiId;
+	private Long batchNumber;
+	private String fileName;
+	private Integer qcStatus;
+	
+	public String getFileName() {
+		return fileName;
+	}
+
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+
+	public UploadDTOForQC(Long uploadId,String uploadUrl,Long userVendorMappingObjectMappingId, Long objectId, Long projectId, String projectName, String objectName,Long ucbiId, Long batchNumber,String fileName,Integer qcStatus) {
+		this.uploadId=uploadId;
+		this.uploadUrl=uploadUrl;
+		this.userVendorMappingObjectMappingId=userVendorMappingObjectMappingId;
+		this.projectId=projectId;
+		this.objectId=objectId;
+		this.projectName=projectName;
+		this.objectName=objectName;
+		this.ucbiId=ucbiId;
+		this.batchNumber=batchNumber;
+		this.fileName=fileName;
+		this.qcStatus=qcStatus;
+	}
+	
+	
+	public Integer getQcStatus() {
+		return qcStatus;
+	}
+
+
+	public void setQcStatus(Integer qcStatus) {
+		this.qcStatus = qcStatus;
+	}
+
+
+	public Long getUcbiId() {
+		return ucbiId;
+	}
+
+
+	public void setUcbiId(Long ucbiId) {
+		this.ucbiId = ucbiId;
+	}
+
+
+	public Long getBatchNumber() {
+		return batchNumber;
+	}
+
+
+	public void setBatchNumber(Long batchNumber) {
+		this.batchNumber = batchNumber;
+	}
+
+
+	public String getProjectName() {
+		return projectName;
+	}
+
+
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+
+
+	public String getObjectName() {
+		return objectName;
+	}
+
+
+	public void setObjectName(String objectName) {
+		this.objectName = objectName;
+	}
+
+
+	public Long getUserVendorMappingObjectMappingId() {
+		return userVendorMappingObjectMappingId;
+	}
+
+
+	public void setUserVendorMappingObjectMappingId(Long userVendorMappingObjectMappingId) {
+		this.userVendorMappingObjectMappingId = userVendorMappingObjectMappingId;
+	}
+
+
+	public Long getProjectId() {
+		return projectId;
+	}
+
+
+	public void setProjectId(Long projectId) {
+		this.projectId = projectId;
+	}
+
+
+	public Long getObjectId() {
+		return objectId;
+	}
+
+
+	public void setObjectId(Long objectId) {
+		this.objectId = objectId;
+	}
+
+
+	public Long getUploadId() {
+		return uploadId;
+	}
+	public void setUploadId(Long uploadId) {
+		this.uploadId = uploadId;
+	}
+	public String getUploadUrl() {
+		return uploadUrl;
+	}
+	public void setUploadUrl(String uploadUrl) {
+		this.uploadUrl = uploadUrl;
+	}
+	
+}
